51c844f2553b5e3d77e71dbd0db185361596606b
Walid Arbouch <a.walidarb@gmail.com>
2025-09-19 08:55:21 +0200

    Add files via upload

---
+++ media.py
@@ -0,0 +1,17 @@
+
+print("Dame el primer número entero:")
+num1 = int(input())
+
+print("Dame el segundo número entero:")
+num2 = int(input())
+
+print("Dame el tercer número entero:")
+num3 = int(input())
+
+print("Dame el cuarto número entero:")
+num4 = int(input())
+
+
+media = (num1 + num2 + num3 + num4) / 4
+
+print(f"La media de los números es: {media:.3f}")
